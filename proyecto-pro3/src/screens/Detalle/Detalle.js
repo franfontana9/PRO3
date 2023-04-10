3989896ab40f6c48c820a3975845209f6c908ec0
franfontana9 <you@example.com>
2023-04-10 10:29:33 -0300

    screen detalle

---
+++ proyecto-pro3/src/screens/Detalle/Detalle.js
@@ -0,0 +1,22 @@
+import { Component } from "react";
+
+class Detalle extends Component {
+    constructor(props){
+        super(props)
+
+    }
+
+    componentDidMount() {
+        // carga info de la api para el detalle
+    }
+
+    render () {
+        return (
+            <>
+            <h1>Detalle de algo</h1>
+            </>
+        )
+    }
+}
+
+export default Detalle
