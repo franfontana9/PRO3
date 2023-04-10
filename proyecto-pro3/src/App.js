0a92520486756d3d9e9d647af02fa50a3eebb825
franfontana9 <you@example.com>
2023-04-10 10:20:24 -0300

    avances app

--- proyecto-pro3/src/App.js
+++ proyecto-pro3/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
-import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
 import Home from './screens/Home/Home'
 import Favoritos from './screens/Favoritos/Favoritos';
 import NotFound from './screens/NotFound/NotFound';
+import Header from './components/Header/Header';
+import Detalle from './screens/Detalle/Detalle'
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Header/>
         <Switch>
-          <Route path='/' exact= {true} component={Home} />
+          <Route path='/'  exact= {true} component={Home} />
           <Route path='/favoritos' component={Favoritos} />
+          <Route path='/detalle/:id' component={Detalle} /> 
           <Route component={NotFound} />
         </Switch>
       <Footer/>
