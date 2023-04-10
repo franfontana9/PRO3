22bf3d1317ca60aa244fdd68604cadecebac116a
santiagoscilingo <santiago.scilingo>
2023-04-10 12:40:45 -0300

    barra busqueda

---
+++ proyecto-pro3/src/components/Buscador/BarraBusqueda.js
@@ -0,0 +1,10 @@
+const BarraBusqueda = () => {
+    return (
+      <form>
+        <input type="search" name="buscador" />
+        <button type="submit">Buscar</button>
+      </form>
+    );
+  };
+  
+  export default BarraBusqueda
