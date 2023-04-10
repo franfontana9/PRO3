a8ab51bc6e30f0e321bc7fbd5a00c32f63861b10
santiagoscilingo <santiago.scilingo>
2023-04-10 12:43:51 -0300

    resultado busqueda

---
+++ proyecto-pro3/src/components/Buscador/ResultadosBusqueda.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class ResultadosBusqueda extends Component {
+  constructor(props) {}
+
+  render() {
+    return (
+      <>
+        <h2>Resultados de la busqueda</h2>
+        <ul></ul>
+      </>
+    );
+  }
+}
+
+export default ResultadosBusqueda
