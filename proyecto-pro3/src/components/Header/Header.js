f4b0292b4f7306bb3d54e9e3692ccf10cf1af9f7
santiagoscilingo <santiago.scilingo>
2023-04-13 17:26:44 -0300

    header css

--- proyecto-pro3/src/components/Header/Header.js
+++ proyecto-pro3/src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import "./header.css";
 
 const Header = () => (
     <header>
         <div className="header-brand">
-            <h1>Nombre Grupo</h1>
-            <img src="#" alt='brand logo'/>
+            <h1>MÜSIK</h1>
+            <img className="header-brand" src="../../img/müsik.jpeg" alt='brand logo'/>
         </div>
         <Navbar/>
     </header>
