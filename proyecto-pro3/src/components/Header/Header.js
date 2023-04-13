ef0d94708a1f14ac313c6d8b1d52cb7d39b1837b
santiagoscilingo <santiago.scilingo>
2023-04-13 17:43:46 -0300

    header fix

--- proyecto-pro3/src/components/Header/Header.js
+++ proyecto-pro3/src/components/Header/Header.js
@@ -1,24 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
-<<<<<<< HEAD
+import ResultadosBusqueda from "../ResultadosBusqueda/ResultadosBusqueda";
 import "./header.css";
 
 const Header = () => (
-    <header>
-        <div className="header-brand">
+    <header className="header-brand">
             <h1>MÜSIK</h1>
             <img className="header-brand" src="../../img/müsik.jpeg" alt='brand logo'/>
-        </div>
-=======
-import ResultadosBusqueda from "../ResultadosBusqueda/ResultadosBusqueda";
-
-const Header = () => (
-    <header className="header-brand">
-        <h1>Nombre Grupo</h1>
-        <img src="#" alt='brand logo'/>
-        <ResultadosBusqueda /> 
->>>>>>> 44e48b0faf859d5b4740c2ba497293e45c5fe347
-        <Navbar/>
+            <ResultadosBusqueda /> 
+            <Navbar/>
     </header>
 )
 
