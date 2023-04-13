44e48b0faf859d5b4740c2ba497293e45c5fe347
franfontana9 <you@example.com>
2023-04-13 17:27:52 -0300

    correcion header

--- proyecto-pro3/src/components/Header/Header.js
+++ proyecto-pro3/src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import ResultadosBusqueda from "../ResultadosBusqueda/ResultadosBusqueda";
 
 const Header = () => (
-    <header>
-        <div className="header-brand">
-            <h1>Nombre Grupo</h1>
-            <img src="#" alt='brand logo'/>
-        </div>
+    <header className="header-brand">
+        <h1>Nombre Grupo</h1>
+        <img src="#" alt='brand logo'/>
+        <ResultadosBusqueda /> 
         <Navbar/>
     </header>
 )
