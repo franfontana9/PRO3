c71fb8f5723bfbbb10d7dbecd3d9b8159b309d27
franfontana9 <you@example.com>
2023-04-13 15:19:34 -0300

    avances resultado de busqueda

--- proyecto-pro3/src/components/ResultadosBusqueda/ResultadosBusqueda.js
+++ proyecto-pro3/src/components/ResultadosBusqueda/ResultadosBusqueda.js
@@ -1,50 +1,70 @@
 import React, { Component } from "react";
+import { API_ENDPOINTS, CATEGORIAS } from "../../constants";
 
 
 class ResultadosBusqueda extends Component {
   constructor(props) {
     super(props)
-    this.setState= {
-      valorInput:'',
+    this.state= {
+      consulta:'',
+      // categoria: CATEGORIAS.album,
+      resultadosBusqueda: []
     }
+    // atamos(bind) nuestra funcion handleOnChange a nuestro componente (esto es generico)
+    this.handleOnChange = this.handleOnChange.bind(this)
   }
 
-  evitarSubmit(event){
-    event.preventDefault()
-    }
-
-  metodoQueFiltra(texto, arrayFuente){
-        let filtrado = arrayFuente.filter((elm)=> elm.name.toLowerCase().includes(texto.toLowerCase()))
-        return filtrado
-    }
-
-  guardarValor(event){
+  handleOnChange(event){
     this.setState(
       {
-        valorInput:event.target.value
-      }, () => {
-          let filtro = this.metodoQueFiltra(this.state.valorInput, this.props.fuente)
-          this.props.actualizador(filtro)
-      }
-        )
-      }  
+        [event.target.name]: event.target.value 
+      })
+      
+  }
 
-  metodoQueEnvia(){
-      console.log('Enviamos la info')
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.consulta !== this.state.consulta) {
+      console.info('consulta', this.state.consulta)
+      const urlBusqueda = `${API_ENDPOINTS.busqueda}${this.state.consulta}`
+  
+      fetch(urlBusqueda)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          resultadosBusqueda: data.data
+        })
+      })
     }
+  }
 
 
   render() {
     return (
-     <form onSubmit={(event)=> this.evitarSubmit(event)}>
-        <div>
-            <label>Busca lo que desees</label>
-        </div>
-        <div>
-            {/* <input onChange={(event)=> this.guardarValor(event)} value={this.state.valorInput}></input> */}
-        </div>
-        <button onClick={() => this.metodoQueEnvia()}>Envar consulta</button>
-     </form>   
+      <>
+        <form onSubmit={this.handleOnSubmit}>
+          <input type="text" name="consulta" placeholder="Buscar 🔍" onChange={this.handleOnChange}/>
+          <button onClick={() => {this.setState({consulta: '', resultados: []})}}>Limpiar</button>            
+        {/*}
+          <select name="categoria" onChange={this.handleOnChange}>
+            <option value={CATEGORIAS.album}>Album</option>
+            <option value={CATEGORIAS.cancion}>Cancion</option>
+          </select>
+        */}
+        </form>   
+
+
+        {this.state.resultadosBusqueda.length > 0  && this.state.consulta != '' && 
+          <section>
+            <h2>Resultados de busqueda</h2>  
+            <ul>
+              {this.state.resultadosBusqueda.map((element) => {
+                return <li key={element.id}>{element.title}</li>
+              })}
+            </ul>
+          </section>
+        } 
+      </>
     )
   }
 
