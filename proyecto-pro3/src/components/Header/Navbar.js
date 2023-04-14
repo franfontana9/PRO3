0385b4b5de2b3574974374758e822a6805476b4a
franfontana9 <you@example.com>
2023-04-14 19:47:33 -0300

    avanes

--- proyecto-pro3/src/components/Header/Navbar.js
+++ proyecto-pro3/src/components/Header/Navbar.js
@@ -16,7 +16,7 @@ const opciones = [
     },
     {
         nombre: 'Albums',
-        path: '/albums'
+        path: '/'
     },
 ]
 
---
+++ proyecto-pro3/src/screens/Canciones/Canciones.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ContenedorCanciones from '../../components/ContenedorCanciones/ContenedorCanciones';
+
+import './mostrar.css'
+
+ class canciones extends Component {
+  render() {
+    return (
+      <ContenedorCanciones/>
+    )
+  }
+}
+
+
+export default canciones
+
